7a659d1b2cbfef589084168f2007481326927fd9
Adesh <adesh022603@gmail.com>
2025-03-08 12:08:27 -0500

    First comm

--- app/page.js
+++ app/page.js
@@ -1,17 +1,32 @@
 import ButtonLogin from "@/components/ButtonLogin";
+import FAQLisItem from "@/components/FAQListItem";
+import Image from "next/image";
+import productDemo from "@/app/productDemo.jpeg";
 
 export default function Home() {
   const isLoggedIn = true;
   const name = "John";
 
+  const pricingFeaturesList = [
+    "Collect customer feedback",
+    "Unlimited boards",
+    "Admin dashboard",
+    "24/7 Support",
+  ];
+
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-base-200 ">
-        <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
+        <div className="max-w-5xl mx-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">CodeFastSaas</div>
           <div className="space-x-4  max-md:hidden">
-            <a className="link link-hover">Pricing</a>
-            <a className="link link-hover">FAQ</a>
+            <a className="link link-hover" href="#pricing">
+              Pricing
+            </a>
+            <a className="link link-hover" href="#faq">
+              FAQ
+            </a>
           </div>
 
           <div className="bg-blue-400">
@@ -20,15 +35,94 @@ export default function Home() {
         </div>
       </section>
 
-      <section className="px-8 text-center py-32 max-w-3xl mx-auto">
-        <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
-          Collect your customer feedback
-        </h1>
-        <div className="opacity-90 mb-10">
-          Create a feedback in minutes, prioritize features, and build products
-          your customers will love.
+      {/* HERO */}
+      <section className="px-8 text-center lg:text-left py-32 max-w-5xl mx-auto flex flex-col lg:flex-row gap-14 items-center lg:items-start">
+        <Image
+          src={productDemo}
+          alt="Product demo"
+          className="w-96 rounded-xl"
+        />
+        <div>
+          {" "}
+          <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">
+            Collect your customer feedback
+          </h1>
+          <div className="opacity-90 mb-10">
+            Create a feedback in minutes, prioritize features, and build
+            products your customers will love.
+          </div>
+          <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
         </div>
-        <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
+      </section>
+
+      {/* PRICING */}
+      <section className="bg-base-200" id="pricing">
+        <div className="px-8 py-32 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+            Pricing
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            A pricing that adapts to your needs
+          </h2>
+
+          <div className="p-8 bg-base-100 w-96 rounded-3xl mx-auto space-y-6">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl font-black">$19</div>
+              <div className="uppercase text-sm font-medium opacity-60">
+                /month
+              </div>
+            </div>
+
+            <ul className="space-y-2">
+              {pricingFeaturesList.map((priceItem) => {
+                return (
+                  <li className="flex gap-2 items-center" key={priceItem}>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      fill="currentColor"
+                      className="text-green-700 w-4 h-4"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                    {priceItem}
+                  </li>
+                );
+              })}
+            </ul>
+
+            <ButtonLogin
+              isLoggedIn={isLoggedIn}
+              name={name}
+              extraStyle="w-full"
+            />
+          </div>
+        </div>
+      </section>
+
+      {/* FAQ */}
+      <section className="bg-base-200" id="faq">
+        <div className="px-8 py-32 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">
+            FAQ
+          </p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            Frequently Asked Questions
+          </h2>
+          <ul className="max-w-lg mx-auto ">
+            {[
+              { question: "QQQQQ111111", answer: "AAAAA1111" },
+              { question: "QQQQQ222", answer: "AAAAA222" },
+              { question: "QQQQQ3333", answer: "AAAAA333" },
+            ].map((qa) => (
+              <FAQLisItem key={qa.question} qa={qa} />
+            ))}
+          </ul>
+        </div>
       </section>
     </main>
   );
---
+++ components/FAQListItem.js
@@ -0,0 +1,44 @@
+"use client";
+
+import { useState } from "react";
+
+const FAQListItem = ({ qa }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <li key={qa.question}>
+      {/*1. Question */}
+      <button
+        className="py-5 font-semibold border-b border-gray-200 w-full text-left flex justify-between items-center"
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <p>{qa.question}</p>
+
+        {isOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="size-4"
+          >
+            <path d="M3.75 7.25a.75.75 0 0 0 0 1.5h8.5a.75.75 0 0 0 0-1.5h-8.5Z" />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 16 16"
+            fill="currentColor"
+            className="size-4"
+          >
+            <path d="M8.75 3.75a.75.75 0 0 0-1.5 0v3.5h-3.5a.75.75 0 0 0 0 1.5h3.5v3.5a.75.75 0 0 0 1.5 0v-3.5h3.5a.75.75 0 0 0 0-1.5h-3.5v-3.5Z" />
+          </svg>
+        )}
+      </button>
+      {/*2. Answer */}
+      <div className={`${isOpen ? "block" : "hidden"} mt-3 mb-6 opacity -90`}>
+        {qa.answer}
+      </div>
+    </li>
+  );
+};
+
+export default FAQListItem;
